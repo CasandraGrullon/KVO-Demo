9baf186072836acdbd9845b5a1cb0a26fbd866e7
Casandra <casandragrullon@pursuit.org>
2020-04-07 12:07:37 -0400

    added color changes based on icons chosen

--- KVO-Demo/WelcomeViewController.swift
+++ KVO-Demo/WelcomeViewController.swift
@@ -32,6 +32,13 @@ class WelcomeViewController: UIViewController {
         iconNameObservation = Settings.shared.observe(\.iconName, options: [.old, .new], changeHandler: { [weak self] (settings, change) in
             guard let newIconName = change.newValue else {return}
             self?.iconImageView.image = UIImage(systemName: "\(newIconName)")
+            if newIconName == "icloud.fill" || newIconName == "globe" {
+                self?.iconImageView.tintColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+                self?.welcomeLabel.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+            } else {
+                self?.iconImageView.tintColor = #colorLiteral(red: 1, green: 0.7171183228, blue: 0, alpha: 1)
+                self?.welcomeLabel.textColor = #colorLiteral(red: 0.8668374419, green: 0.5093212128, blue: 0, alpha: 1)
+            }
         })
     }
     
